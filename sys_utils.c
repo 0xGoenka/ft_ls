32e6fddc84419572287eacb5514e3b43689664fa
Esteban LECLET <eleclet@e2r2p3.42.fr>
2016-01-26 11:23:18 +0100

    sd

--- sys_utils.c
+++ sys_utils.c
@@ -6,7 +6,7 @@
 /*   By: eleclet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/09 15:51:29 by eleclet           #+#    #+#             */
-/*   Updated: 2016/01/13 15:44:36 by eleclet          ###   ########.fr       */
+/*   Updated: 2016/01/25 16:48:32 by eleclet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,44 +33,28 @@ char	*ufid(uid_t uid) // user from id
 	g = getgrgid(gid);
 	return (g->gr_name);
 }
-char	*parse_right(char *s)
+int		findmax(t_lst *lst, int i)
 {
-	char *ret;
+	int ret;
 
-	int i;
-
-	i = 0;
-	printf("s = %s\n",s);
-	while (i < 15)
+	ret = 0;
+	if (i)
 	{
-		s[i] = (s[i] == '1')? 'x' : '-',i++;
-		s[i] = (s[i] == '1')? 'w' : '-',i++;
-		s[i] = (s[i] == '1')? 'r' : '-',i++;
+		while (lst)
+		{
+			if (ret < nu_len(lst->info.nblink, 10))
+				ret = nu_len(lst->info.nblink, 10);
+			lst = lst->next;
+		}
 	}
-	s = ft_strrvrs(s);
-	printf("\n s rvrs = %s", s);
-	ret = s;
-	printf("\n ret sub = %s\n", ret);
-	s[5] = (s[0] == '1')? '-' : 'd';
-	ret = ft_strdup(s + 5);
+	else
+	{
+		while (lst)
+		{	
+			if (ret < nu_len(lst->info.size,10))
+				ret = nu_len(lst->info.size,10);
+			lst = lst->next;
+		}
+	}
 	return (ret);
-}
-char	*dec_to_bin(int n)
-{
-	char *s;
-	int i;
-
-	i = 0;
-	s = (char *)malloc(sizeof(char) * 16 + 1);
-	//printf("n = %d\n", n);
-	while (n)
-	{
-		if (n & 1)
-			s[i] = '1';
-		else
-			s[i] = '0';
-		i++, n >>= 1;
-	}
-	s[i] = '\0';
-	return (s);
 }
--- test.c
+++ test.c
@@ -6,21 +6,19 @@
 /*   By: eleclet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 13:56:52 by eleclet           #+#    #+#             */
-/*   Updated: 2016/01/13 14:37:04 by eleclet          ###   ########.fr       */
+/*   Updated: 2016/01/25 18:10:48 by eleclet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ls.h"
 
-int main()
+int main(int argc,char **argv)
 {
 	t_lst *liste;	
 	t_lst *ptr;
-	liste = getinfo("libft");
+	liste = getinfo(argv[1]);
 	ptr = liste;
-	while (liste)
-	{
-		liste = printlist(liste);
-	}
+	print(liste->next);
+	//printlist(liste->next,findmax(liste->next, 1) , findmax(liste->next, 0));
 	return (0);
 }
